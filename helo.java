bc94f159c00b66a7e5b29ccc85f567ba99d2268d
mani <mailtomani03siem@gmail.com>
2021-05-01 12:32:14 +0000

    fourth change

--- helo.java
+++ helo.java
@@ -2,9 +2,9 @@ public class Hello{
 
 	public static void main(String[] args){
 
-		for(i=1;i<=10;i++){
+		for(int i=1;i<=10;i++){
 
-			System.out.println("Hello world";+i);
+			System.out.println("Hello world....";+i);
 		}
 	}
 }
