5182a5d7be63feb1a0ee81bd1fdd87a05599a32c
mani <mailtomani03siem@gmail.com>
2021-05-01 12:28:47 +0000

    third change

--- helo.java
+++ helo.java
@@ -1,6 +1,9 @@
 public class Hello{
-	public static void mani(String[] args){
+
+	public static void main(String[] args){
+
 		for(i=1;i<=10;i++){
+
 			System.out.println("Hello world";+i);
 		}
 	}
